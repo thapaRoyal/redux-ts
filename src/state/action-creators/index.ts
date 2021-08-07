be5b3a91936a64c169a2e7ec6d42838ea75eb550
thapaRoyal <thaparoyal17@gmail.com>
2021-08-07 17:36:28 +0545

    adding request login

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -3,12 +3,28 @@ import axios from "axios";
 import { ActionType } from "../action-types/index";
 import { Action } from "../actions/index";
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
   return async (dispatch: any) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
     try {
+      const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (error) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
